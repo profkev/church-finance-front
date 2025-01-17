4e978cce8a469614a438a8e289c2941d3df0e626
profkev <kmaina102@gmail.com>
2025-01-17 17:36:25 -0500

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
+    <footer className="bg-gray-800 text-white p-4 text-center w-full fixed bottom-0 left-0">
       <p>&copy; {new Date().getFullYear()} Church Finance. All rights reserved.</p>
     </footer>
   );
